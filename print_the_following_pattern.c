b5f5e3678ff9d1f67a122e41086346f94fee4aba
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-10-16 14:36:51 +0530

    #include <stdio.h> int main() {    int i, j, rows;    scanf("%d", &rows);    for (i = 1; i <= rows; ++i) {       for (j = 1; j <= i; ++j) {          printf("%d", j);       }       printf(" ");    }    return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,13 @@
-#include<stdio.h>
-int main()
-{
-    int i,j,n;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        for(j=n;j>=1;j--)
-        {
-            printf("%d ",j);
-        }
-        printf("
+#include <stdio.h>
+int main() {
+   int i, j, rows;
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i) {
+      for (j = 1; j <= i; ++j) {
+         printf("%d", j);
+      }
+      printf("
 ");
-    }
+   }
+   return 0;
 }
